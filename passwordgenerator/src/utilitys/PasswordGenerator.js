5e408afd82c225f96b229fb7cd534e4d41c34c2c
Chris Urbano <ChrisUrbano1116@gmail.com>
2020-12-08 14:09:20 -0700

    renovating

---
+++ passwordgenerator/src/utilitys/PasswordGenerator.js
@@ -0,0 +1,67 @@
+export class RandomPassword {
+    constructor() {
+        this.characters = "";
+    }
+    setUpperCase(isUpperCase){
+        if (isUpperCase) {
+            this.characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        }
+        return this
+    }
+
+    setLowerCase(isLowerCase){
+        if (isLowerCase) {
+            this.characters += "abcdefghijklmnopqrstuvwxyz"
+        }
+        return this
+    }
+
+    setLength(length) {
+        this.length = length;
+        return this;
+    }
+    setNumbers(isNumeric) {
+        if (isNumeric) {
+          this.characters += "0123456789";
+        }
+        return this;
+    }
+
+    setSymbol(isSymbolic) {
+        if (isSymbolic) {
+          this.characters += "!@$%^&*()<>,.?/[]{}-=_+";
+        }
+        return this;
+    }
+
+
+    generatePassword(){
+
+        let characterList = this.characters;
+        if(characterList <= 0){
+            return "Hey dont worry you will get there eventually";
+        }
+        let password = "";
+        for (let i = 0; i < this.length; ++i) {
+            password += characterList[getRandomInt(0, characterList.length - 1)];
+        }
+        return password;
+    }
+
+    getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+
+    
+
+
+   
+
+
+        
+
+    
+        
+
+}
