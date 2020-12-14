e06bb55fa98c053bb2433335eeb221a9fb153d28
nkiv <no.kivett@gmail.com>
2020-12-14 10:45:23 -0700

    framework for checkbox

--- passwordgenerator/src/components/CheckBox.js
+++ passwordgenerator/src/components/CheckBox.js
@@ -1,9 +1,24 @@
 import {React ,useState} from 'react';
 import './CheckBox.css';
+import Switch from '@material-ui/core/Switch';
 
-const []
 const CheckBox = () => {
+    const [isCheck, setIsCheck] = useState(false);
+    
+    const handleClick = () => {
+        setIsOpen(!isCheck);
+    }
 
+return(
+<div>
+    <Switch
+        checked={state.isCheck}
+        onChange={handleClick}
+        name="checkedA"
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+    />
+</div>  
+    );
 }
 
 export default CheckBox;
